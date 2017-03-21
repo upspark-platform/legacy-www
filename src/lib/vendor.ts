511ec65f0d5a553fccdc867e230a94b855476ea2
David Rehmat <me@dave.blue>
2017-03-20 22:28:31 -0400

    added bootstrap 4

--- src/lib/vendor.ts
+++ src/lib/vendor.ts
@@ -9,5 +9,14 @@ import '@angular/router';
 // RxJS
 import 'rxjs';
 
-// HammerJS
-import 'hammerjs';
+// JQuery
+const $ = require('vendor/bootstrap/scripts/jquery.min.js');
+window["jQuery"] = $;
+window["$"] = $;
+
+// Tether
+window["Tether"] = require('vendor/bootstrap/scripts/tether.min.js');
+
+// Bootstrap
+import 'vendor/bootstrap/scripts/toolkit';
+
