5d5255304034898094e28fbd1fb367f9ad1294cc
David Rehmat <me@dave.blue>
2021-04-13 04:10:45 -0400

    prep

--- src/app/feedback/feedback-routing.module.ts
+++ src/app/feedback/feedback-routing.module.ts
@@ -6,35 +6,7 @@ import {FeedbackErrorComponent} from "./feedback-error/feedback-error.component"
 
 @NgModule({
     imports: [
-        RouterModule.forChild([
-            {
-                path: 'feedback',
-                component: FeedbackComponent,
-                data: {
-                    meta: {
-                        title: 'Upspark - Feedback',
-                    }
-                }
-            },
-            {
-                path: 'feedback/success',
-                component: FeedbackSuccessComponent,
-                data: {
-                    meta: {
-                        title: 'Upspark - Feedback',
-                    }
-                }
-            },
-            {
-                path: 'feedback/error',
-                component: FeedbackErrorComponent,
-                data: {
-                    meta: {
-                        title: 'Upspark - Feedback',
-                    }
-                }
-            },
-        ])
+
     ],
     exports: [
         RouterModule
